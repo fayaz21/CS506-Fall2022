e7d663182a42b45e79c1cdd2b96a0e1ab0dcc69d
fayaz21 <113390647+fayaz21@users.noreply.github.com>
2022-09-15 12:58:55 -0400

    Update safety.py

--- labs/01-lab/safety/safety.py
+++ labs/01-lab/safety/safety.py
@@ -3,7 +3,26 @@ from .hospital import draw_hospital
 from .policestation import draw_policestation
 
 def draw_safety():
-    draw_firestation()
-    draw_hospital()
-    draw_policestation()
+    
+    def draw_firestation():
+        for i in range (1,20):
+            for j range (1,20):
+                print("**")
     return
+            
+        
+    def draw_hospital():
+        for i in range (1,20):
+            print("**")
+            for j range (1,20):
+                print("**")
+    return
+                
+    def draw_policestation():
+        for i in range (1,20):
+            print("**")
+            for j range (1,20):
+                print("**")
+    return
+
+return
